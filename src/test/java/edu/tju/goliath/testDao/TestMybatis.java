d665bf2537b69eb2ef1285df8dcc513e5ddffa44
MorriganMesser <1053172997@qq.com>
2016-09-21 21:18:34 +0800

    web structure page

--- src/test/java/edu/tju/goliath/testDao/TestMybatis.java
+++ src/test/java/edu/tju/goliath/testDao/TestMybatis.java
@@ -33,7 +33,7 @@ public class TestMybatis {
 
 	@Test
 	public void test1() {
-		User u = userService.getUserById("1");
+		User u = userService.getUserById("2");
 		logger.info(JSON.toJSONStringWithDateFormat(u, "yyyy-MM-dd HH:mm:ss"));
 	}
 
