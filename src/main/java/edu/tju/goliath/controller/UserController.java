9f533e5b49997fbdac2b3225f1ddc2a52850701b
tianhongce <1196585084@qq.com>
2016-09-22 11:13:42 +0800

    add login

--- src/main/java/edu/tju/goliath/controller/UserController.java
+++ src/main/java/edu/tju/goliath/controller/UserController.java
@@ -1,11 +1,17 @@
 package edu.tju.goliath.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.google.code.kaptcha.Constants;
 
 import edu.tju.goliath.entity.User;
 import edu.tju.goliath.service.UserServiceI;
@@ -39,15 +45,44 @@ public class UserController {
 		return "register_success";
 	}
 	
-	@RequestMapping("loginUser")
-	public String loginUser(@PathVariable User user, HttpServletRequest request){
-		User user_data=userService.selectByUsername(user.getUsername());
-		if(user.getUsername()==user_data.getUsername()){
-			return "login_success";
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(HttpServletRequest request,
+			@RequestParam("username") String username,
+			@RequestParam("userpwd") String userpwd,
+			@RequestParam("imgcode") String imgcode,ModelMap model) {
+		System.out.println("start request");
+		HttpSession session = request.getSession();
+		String code = (String) session
+				.getAttribute(Constants.KAPTCHA_SESSION_KEY);
+		System.out.println(code);
+		String msg="";
+		if(!imgcode.equals(code)){
+			 msg="验证码错误！";
+			 System.out.println(msg);
+			session.setAttribute("msg", msg);
+//			model.addAttribute("msg", msg);
+			return "views/login";
 		}else{
-			return "login_error";
+			if(userService.selectByUsername(username)==null){
+				 msg="此用户不存在！";
+				 System.out.println(msg);
+				session.setAttribute("msg", msg);
+				return "views/login";
+			}else{
+				if(!userpwd.equals(userService.selectByUsername(username).getUserpwd())){
+					msg="密码错误！";
+					System.out.println(msg);
+					session.setAttribute("msg", msg);
+					return "views/login";
+				}else{
+					User user=userService.selectByUsername(username);
+					System.out.println(user.getUsername());
+					session.setAttribute("user", user);
+					return "views/login_success";
+				}
+			}
 		}
-		
-		
+
+
 	}
 }
