6555fd767e2cc4fee11941bb323ef80f861baada
MorriganMesser <1053172997@qq.com>
2016-09-23 17:55:00 +0800

    Function v0.0

---
+++ src/main/java/edu/tju/goliath/util/TestPaper.java
@@ -0,0 +1,8 @@
+package edu.tju.goliath.util;
+public class TestPaper {
+	public static void main(String args[]){
+		ExpressionClass Exp1 = new ExpressionClass();
+		Exp1.createExp(10);
+		Exp1.showExp();
+	}
+}
