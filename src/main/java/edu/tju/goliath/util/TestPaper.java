3f1097df1498ad0ecd222e9d6ddccc1f724ccafa
kedixa <1204837541@qq.com>
2016-09-27 21:20:32 +0800

    remove unused

--- src/main/java/edu/tju/goliath/util/TestPaper.java
+++
@@ -1,8 +0,0 @@
-package edu.tju.goliath.util;
-public class TestPaper {
-	public static void main(String args[]){
-		ExpressionClass Exp1 = new ExpressionClass();
-		Exp1.createExp(10);
-		Exp1.showExp();
-	}
-}
