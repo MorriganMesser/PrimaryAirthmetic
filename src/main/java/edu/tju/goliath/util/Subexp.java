43cbbd02928034246e1d3b0f154d8b83380d858e
kedixa <1204837541@qq.com>
2016-10-08 16:25:45 +0800

    subexpr class

--- src/main/java/edu/tju/goliath/util/Subexp.java
+++ src/main/java/edu/tju/goliath/util/Subexp.java
@@ -1,5 +1,25 @@
 package edu.tju.goliath.util;
 
 public class Subexp {
-
+	/*
+	 * public class Subexp:
+	 * 用于辅助生成表达式。
+	 */
+	private String expr;
+	private Fraction result;
+	
+	public Subexp(String expr, Fraction result) {
+		this.expr = expr;
+		this.result = result;
+	}
+	public Subexp() {
+		this(new String(), new Fraction());
+	}
+	
+	public String getExpr() {
+		return this.expr;
+	}
+	public Fraction getResult() {
+		return this.result;
+	}
 }
